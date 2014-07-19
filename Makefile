b652112636945eb90d1c2fa3bab0d602fef6d74c
Malcolm Locke <malc@wholemeal.co.nz>
2014-07-19 23:56:38 +1200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: eta_car
+
+eta_car:
+	chmod 644 site/eta_car_periastron_2014/fits/*
+	./generate.py eta_car_periastron_2014 'Eta Carinae periastron campaign 2014'
+	zip -r -j site/eta_car_periastron_2014.zip site/eta_car_periastron_2014/fits/
+
+sync:
+	rsync -vax site/ bollo:websites/saser.wholemeal.co.nz/
---
+++ generate.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+from jinja2 import Environment, FileSystemLoader
+import os
+import sys
+import pyfits
+import dateutil
+from specreduce.specreduce import BessSpectra
+
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('campaign.html')
+
+site_dir = 'site'
+campaign_dir = sys.argv[1]
+title = sys.argv[2]
+
+fits_dir = os.path.join(campaign_dir, 'fits')
+index_html = os.path.join(site_dir, campaign_dir, 'index.html')
+zipfilename = campaign_dir + '.zip'
+
+files = [os.path.join(fits_dir, f) for f in os.listdir(os.path.join(site_dir,fits_dir))]
+hdulists = [pyfits.open(os.path.join(site_dir, f)) for f in files]
+spectra = [BessSpectra(hdulist) for hdulist in hdulists]
+sorted_spectra = sorted(
+        zip(files, spectra), key=lambda tup: tup[1].header()['DATE-OBS'])
+
+with open(index_html, 'w') as fh:
+    fh.write(template.render(
+        title=title, spectra=sorted_spectra, zipfilename=zipfilename))
